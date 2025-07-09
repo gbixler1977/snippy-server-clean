de3b7f2082d9e355e8e2dfe3fe9bb45618a2c9d1
gbixler1977 <gene.bixler@gmail.com>
2025-07-09 11:09:18 -0400

    Adding CORS policy

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const { v4: uuidv4 } = require('uuid');
 const { addDonor, getCodeByEmail, isCodeValid, isCodeValidForEmail, isAdmin } = require('./db');
 
 const app = express();
+
+// Allow CORS from Chrome Extensions
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  // Only allow Chrome Extension origin
+  if (origin && origin.startsWith("chrome-extension://")) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
+
+
 app.use(express.json());
 
 // Health check
