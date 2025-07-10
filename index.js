b429413f1c754a3c0c56582fc127896bacdc7250
gbixler1977 <gene.bixler@gmail.com>
2025-07-10 11:13:41 -0400

    Update CORS

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use((req, res, next) => {
   // }
 
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, auth");
+
 
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
