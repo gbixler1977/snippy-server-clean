33c38cae2fe6b5a0868111f545fd261a12568fcd
gbixler1977 <gene.bixler@gmail.com>
2025-07-10 12:28:53 -0400

    Bug fix

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('/mnt/data/donors.db');
+const db = new sqlite3.Database('/data/donors.db');
 
 // Create table if not exists
 db.serialize(() => {
